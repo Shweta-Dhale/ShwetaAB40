a0db858afeee83e3d2222f1648f5c6c0c8a973c2
Shweta <shwetadhale@gmail.com>
2024-04-16 17:34:12 +0530

    ConstructorExample

---
+++ ConstructorProg.java
@@ -0,0 +1,28 @@
+package javapackage;
+
+public class ConstructorProg {
+	
+	int age= 10;
+	char gen= 'M';
+
+
+	public static void main(String[] args) {
+		
+		ConstructorProg c1= new ConstructorProg();
+		
+	}
+	
+	
+	ConstructorProg()
+	
+	{
+		
+			
+		System.out.println("Age is:"+age);
+		
+			
+		System.out.println("Gender is:"+gen);
+			
+			
+	}
+}
